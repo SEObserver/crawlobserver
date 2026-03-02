5bccf24d14575d6db5078b2ce4d4d48bcbeab4be
512banque <k@seobserver.com>
2026-03-02 13:05:20 +0400

    Add DMG creation step to local build script

--- build-app.sh
+++ build-app.sh
@@ -21,4 +21,15 @@ cp build/darwin/Info.plist "${BUNDLE}/Contents/Info.plist"
 cp "${APP_NAME}" "${BUNDLE}/Contents/MacOS/${APP_NAME}"
 cp build/darwin/iconfile.icns "${BUNDLE}/Contents/Resources/iconfile.icns"
 
-echo "==> Done! Run with: open ${BUNDLE}"
+echo "==> Creating DMG..."
+DMG_NAME="${APP_NAME}.dmg"
+DMG_STAGING="build/dmg-staging"
+rm -rf "${DMG_STAGING}" "build/${DMG_NAME}"
+mkdir -p "${DMG_STAGING}"
+cp -R "${BUNDLE}" "${DMG_STAGING}/"
+ln -s /Applications "${DMG_STAGING}/Applications"
+hdiutil create "build/${DMG_NAME}" -volname "${APP_NAME}" -srcfolder "${DMG_STAGING}" -format UDZO -ov
+rm -rf "${DMG_STAGING}"
+
+echo "==> Done! DMG: build/${DMG_NAME}"
+echo "    Or run directly: open ${BUNDLE}"
